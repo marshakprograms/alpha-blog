a0ef99a4ec84ab02b17c2baa558f68ac52363e3e
marshakprograms <marshakprogrammer@gmail.com>
2022-12-05 00:41:12 +0000

    added article listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
   
   def show
-    
-    
     # turn the above into an instance variable
     @article = Article.find(params[:id])
   end
   
+  def index
+    @articles = Article.all
+  end
+
 end
