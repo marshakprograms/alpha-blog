40196ac542ffb33058f38d48a338dca0e0491f4e
marshakprograms <marshakprogrammer@gmail.com>
2022-12-05 11:37:27 +0000

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,5 +8,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  
+  def new
+  end
+  
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    @article.save
+  end
+
 
 end
