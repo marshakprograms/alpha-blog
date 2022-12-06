3b160fca19fa8c29cb74f27736659ccfb0a559cf
marshakprograms <marshakprogrammer@gmail.com>
2022-12-06 15:10:51 +0000

    commented out description of form creation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,5 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "dd256a417a414ca2bab1054b6a663b3c.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "9736857e2e59493fb1f327d7e908c7e4.vfs.cloud9.us-east-1.amazonaws.com"
 end
